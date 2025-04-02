8975c0a139f503487ae1d3ba7392024d54fd28ca
Hee48 <lvovl7070@naver.com>
2025-04-02 15:24:55 +0900

    Initial Commit

---
+++ GGParking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GGParking
+//
+//  Created by Hee  on 4/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
